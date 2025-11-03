87d3bae37b242343ae6e832cc39d8471fdc51ea0
premchand505 <prem595pop@gmail.com>
2025-11-03 12:21:28 +0530

    fix:edited model as gemini-2.5-pro-preview-05-06

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -17,7 +17,7 @@ if (!GEMINI_API_KEY) {
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-exp",
+  model: "gemini-2.5-pro-preview-05-06",
 });
 const generationConfig: GenerationConfig = {
   temperature: 0.8,
